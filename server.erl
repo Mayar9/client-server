1bc525cd00372d1fa1b85fb27723c07605defaa1
Mayar Hassan <mayar7assan567@gmail.com>
2020-05-07 16:44:14 +0200

    Update server.erl

--- server.erl
+++ server.erl
@@ -8,19 +8,19 @@ start() ->
 init() ->
     register(server,self()),
     Resources = hw:get_resources(),
-    io:format("hna############## ~p ~n",[Resources]),
+    io:format("############## ~p ~n",[Resources]),
     loop(Resources).
 
 loop(ResDB) ->
     receive
 	{request, _From, alloc} ->
 	    {Resource, NewResDB} = hw:alloc(ResDB),
-	    io:format("hank############## ~p ~n",[Resource]),
+	    io:format("############## ~p ~n",[Resource]),
 	    _From ! {reply, Resource},
 	    loop(NewResDB);
 	{request, _From, {free, Resource}} ->
 	    NewResDB = hw:free(Resource, ResDB),
-	    io:format("yarab ############# ~p ~n",[NewResDB]),
+	    io:format("############# ~p ~n",[NewResDB]),
 	    _From ! {replay, ok},
 	    loop(NewResDB);
 	_Other ->
